1b772822ac69c62a384e476209a69bdb13355a1c
Glen-tech <glen_vp@hotmail.com>
2021-11-06 15:51:25 +0100

    Problem HTTP request
    - Adding http request
    - Probably firewall issue

--- website/__init__.py
+++ website/__init__.py
@@ -4,11 +4,8 @@ def create_app():
     app= Flask(__name__)
     app.config['SECRET_KEY'] = 'Here can you give a secret key' # secret key for the webb appilaction
     
-    
-    from .views import views
     from .auth import auth
     
-    app.register_blueprint(views, url_prefix='/') # register routes
     app.register_blueprint(auth,  url_prefix='/') # / means no prefix
     
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,21 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request
 from flask_login import current_user
 #routes for or website
 
 auth = Blueprint('auth', __name__)
 
+#user is manditory, will crash if not
 
-
-@auth.route('/login')
-def login():
-    return render_template("login.html" ,  user = current_user) #user is manditory, will crash if not
+@auth.route('/')
+def home():
+    return render_template("home.html", user = current_user) 
     
-  
+
 @auth.route('/logout')  
 def logout():
     return render_template("logout.html",  user = current_user) 
     
     
-@auth.route('/sign-up')
+@auth.route('/login',methods=['POST,GET']) # will now handle post and get requests
+def login():
+    #data = request.form
+    #print(data)
+    return render_template("login.html" ,  user = current_user)
+    
+        
+@auth.route('/sign-up',methods=['POST,GET'])
 def sign_up():
-     return render_template("sign_up.html",user = current_user) 
+    return render_template("sign_up.html",user = current_user) 
