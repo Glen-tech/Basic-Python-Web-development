8a17a442ba8173fb39a3a8f176ef77e87a5aa3f2
Glen-tech <glen_vp@hotmail.com>
2021-11-07 20:49:07 +0100

    Signed form handeling
    - HTTP request works
    - Variable are succesfully added with the form input
    - Jinga added for CSS in the form use
    
    Next step database

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, render_template,request
-from flask_login import current_user
+from flask import Blueprint, render_template,request,flash
 #routes for or website
 
 auth = Blueprint('auth', __name__)
@@ -8,21 +7,44 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/')
 def home():
-    return render_template("home.html", user = current_user) 
+    return render_template("home.html") 
     
 
 @auth.route('/logout')  
 def logout():
-    return render_template("logout.html",  user = current_user) 
+    return render_template("logout.html") 
     
-    
-@auth.route('/login',methods=['POST,GET']) # will now handle post and get requests
+@auth.route('/login',methods=['GET', 'POST']) # will now handle post and get requests
 def login():
-    #data = request.form
-    #print(data)
-    return render_template("login.html" ,  user = current_user)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print(email)
+        print(password)
+    return render_template("login.html")
     
         
-@auth.route('/sign-up',methods=['POST,GET'])
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return render_template("sign_up.html",user = current_user) 
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        print(email)
+        print(firstName)
+        print(password1)
+        print(password2)
+        
+        if(len(email) < 3):
+            flash("Email must be greater than 2 characters", category = 'error')
+        elif(len(firstName) < 3):
+            flash("First name must be greater than 2 characters", category = 'error')
+        elif(len(password1) < 5):
+            flash("password must be greater than 4 characters", category = 'error')
+        elif(password1 != password2):
+            flash("Passwords don\'t match, try again", category = 'error')
+        else:
+            flash("Account succesfully created", category = 'succes')
+            
+    return render_template("sign_up.html")
